546493ee3d1500c713d54c036814fe6cd757d49d
Fluo-xetine <55126897+Fluo-xetine@users.noreply.github.com>
2021-12-15 17:47:16 +0800

    Delete javaSE_07 directory

--- javaSE_07/Company.java
+++
@@ -1,8 +0,0 @@
-package com.javaSE_07;
-
-public class Company {
-    public void show(ColaEmployee emplory,int month){
-        double salary=emplory.getSalary(month);
-        System.out.println(emplory.name+" "+month+"月份的工资为 "+salary);
-    }
-}
